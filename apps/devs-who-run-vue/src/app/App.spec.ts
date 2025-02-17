428a55789bf38bd502b057829f808d61fc562ac2
Sashikumar <yshashi30@gmail.com>
2025-02-17 23:37:32 +0800

    fix: updated the test case

--- apps/devs-who-run-vue/src/app/App.spec.ts
+++ apps/devs-who-run-vue/src/app/App.spec.ts
@@ -1,10 +1,23 @@
 import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 describe('App', () => {
   it('renders properly', async () => {
-    const wrapper = mount(App, {});
-    expect(wrapper.text()).toContain('Switch to dark mode Devs Who Run');
+    const router = createRouter({
+      history: createWebHistory(),
+      routes: []
+    });
+
+    const wrapper = mount(App, {
+      global: {
+        plugins: [router]
+      }
+    });
+
+    expect(wrapper.find('.min-h-screen').exists()).toBe(true);
+    expect(wrapper.find('.bg-white').exists()).toBe(true);
+    expect(wrapper.findComponent({ name: 'ThemeButton' }).exists()).toBe(true);
   });
 });
